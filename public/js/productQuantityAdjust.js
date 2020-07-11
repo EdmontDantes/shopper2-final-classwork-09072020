c43ed746b430a6fdefa4d7f4460643505f3329ee
Bogdan Kowaltchook <bogdan@En760.kowaltchook.com>
2020-07-11 18:17:19 -0400

    refactored for DRY public/js/productQuantityAdjust.js

--- public/js/productQuantityAdjust.js
+++ public/js/productQuantityAdjust.js
@@ -1,40 +1,38 @@
+const agnosticEventFunc = (event) => {
+    event.preventDefault();
 
-if(document.getElementById('plus')) {
-    document.getElementById('plus').addEventListener('click', (event) => {
-        event.preventDefault();
-
-        let priceValue = parseFloat(document.getElementById('priceValue').value)
-        let quantity = parseInt(document.getElementById('quantity').value)
-        let priceHidden = parseFloat(document.getElementById('priceHidden').value)
-
+    let priceValue = parseFloat(document.getElementById('priceValue').value);
+    let quantity = parseInt(document.getElementById('quantity').value);
+    let priceHidden = parseFloat(document.getElementById('priceHidden').value);
+    
+    if (event.target.id === 'plus') {
         priceValue += priceHidden;
         quantity += 1;
-
-        document.getElementById('quantity').value = quantity;
-        document.getElementById('priceValue').value = priceValue.toFixed(2);
-        document.getElementById('total').innerHTML = quantity;
-    })
-}
-
-if(document.getElementById('minus')) {
-    document.getElementById('minus').addEventListener('click', (event) => {
-        event.preventDefault();
-
-        let priceValue = parseFloat(document.getElementById('priceValue').value)
-        let quantity = parseInt(document.getElementById('quantity').value)
-        let priceHidden = parseFloat(document.getElementById('priceHidden').value)
-
+    
+    } else if (event.target.id === 'minus') {
+        
         if(quantity === 1) {
             quantity = 1;
             priceValue = priceHidden;
             return;
         } else {
+            
             priceValue -= priceHidden;
             quantity -= 1;
         }
+    }
+    
 
-        document.getElementById('quantity').value = quantity;
-        document.getElementById('priceValue').value = priceValue.toFixed(2);
-        document.getElementById('total').innerHTML = quantity;
-    })
+    document.getElementById('quantity').value = quantity;
+    document.getElementById('priceValue').value = priceValue.toFixed(2);
+    document.getElementById('total').innerHTML = quantity;
+}
+
+
+if(document.getElementById('plus')) {
+    document.getElementById('plus').addEventListener('click', agnosticEventFunc)
+}
+
+if(document.getElementById('minus')) {
+    document.getElementById('minus').addEventListener('click', agnosticEventFunc)
 }
